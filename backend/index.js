d7fd431d76091f3c63b377c2f595daad89e1e2eb
Captain-b <babakrezai@hotmail.com>
2020-09-19 00:28:06 +0100

    Register public and private routes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
+import RegisterRoutes from './src/routes/api';
+import RegisterPublicRoutes from './src/routes/public'
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', function(req, res) {
-	res.json({test: 'hello'});
-});
+RegisterRoutes(app);
+RegisterPublicRoutes(app);
 
 const port = 5000;
 
 app.listen(port, async function() {
 	console.log(`Server running on port: ${port}`);
-})
+});
