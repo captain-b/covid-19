dafae15338826fceac70b57ad70f9680eba20207
Captain-b <babakrezai@hotmail.com>
2020-09-19 20:27:35 +0100

    Add onChange listener to the dropdown box and set the selected country

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
   const [countries, setCountries] = useState([{country: "Country"}]);
+  const [selectedCountry, setSelectedCountry] = useState('worldwide');
 
   useEffect(() =>  {
 
@@ -12,7 +13,7 @@ function App() {
       try {
         const countryList = await (await fetch('/get_countries')).json();
         setCountries(countryList); // Set our countries object to our country list array.
-      } catch (error) {
+      } catch (error) { // TODO: Rework this logic.
         alert('There was a problem fetching some data. please try again later.');
         console.log("there was an error", error);
       }
@@ -22,6 +23,11 @@ function App() {
 
   }, []);
 
+  const countryChange = async (e) => {
+    const countryCode = e.target.value;
+    setSelectedCountry(countryCode);
+  }
+
   return (
     <div className="app">
       <div className="app__header">
@@ -29,8 +35,8 @@ function App() {
         <h1>Covid-19 tracker app.</h1>
         {/* Create a dropdown box to display country names and codes*/}
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="worldwide">
-          <MenuItem value="worldwide">WorldWide</MenuItem>
+          <Select variant="outlined" value={selectedCountry} onChange={countryChange}>
+          <MenuItem value="worldwide">World Wide</MenuItem>
           {
             countries.map(country => (
               /* <MenuItem value={country.country}>{country.iso3}</MenuItem> */
