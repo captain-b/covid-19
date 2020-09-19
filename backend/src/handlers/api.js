0a08e05b8ac8f5b0a230b0b98066230ea04a8ff3
Captain-b <babakrezai@hotmail.com>
2020-09-19 10:26:52 +0100

    Create private and public handlers

--- backend/src/handlers/api.js
+++ backend/src/handlers/api.js
@@ -1,3 +1,19 @@
+import Request from '../utils/request'
+
 export const Home = async function (req, res) {
+	try {
+		console.log('test');
+		const test = await Request.get('countries');
+		test.map((country) => {
+			if (country.countryInfo.iso3 === null)
+				console.log(country);
+			// else
+			// 	console.log(country.countryInfo.iso3);
+		});
+	} catch (error) {
+		console.log('test2');
+		console.log(error);
+	}
+
     res.send('hi');
 };
