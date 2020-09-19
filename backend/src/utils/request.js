91a2843a5993f526ba881a266b9d11fa2571a47d
Captain-b <babakrezai@hotmail.com>
2020-09-19 10:30:28 +0100

    Create request utility to handle https calls

---
+++ backend/src/utils/request.js
@@ -0,0 +1,45 @@
+const https = require('https');
+
+const deseasesURL = 'https://disease.sh/v3/covid-19';
+
+export default class Request {
+	static async get(url, params) {
+		try {
+			// Convert params to correct format
+			// const stringParams = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+			// Create the full URL
+			const fullURL = `${deseasesURL}/${url}`;
+			// Send the request
+			const response = await request(fullURL);
+			return (response);
+		} catch (error) {
+			throw error;
+		}
+	}
+}
+
+function request(endpoint) {
+	return new Promise((resolve, reject) => {
+		// Make a GET request
+		https.get(endpoint, (resp) => {
+		  let data = '';
+
+		  // A chunk of data has been recieved.
+		  resp.on('data', (chunk) => {
+		    data += chunk;
+		  });
+
+		  // The whole response has been received.
+		  resp.on('end', () => {
+		  	// Convert to JSON
+		  	const response = JSON.parse(data);
+		  	// Resolve the promise
+		    return resolve(response);
+		  });
+
+		}).on("error", (error) => {
+			// Reject the promise
+			return reject(error);
+		});
+	});
+}
