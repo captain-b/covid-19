4692b44149c46be5dcd5eb1d5a8f3e59c1f0e730
Captain-b <babakrezai@hotmail.com>
2020-09-20 13:54:15 +0100

    Add historic data route and handler

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,6 +1,6 @@
 import {
     CountryList, WorldWideData,
-	CountryData
+	CountryData, HistoricData
 } from "../handlers/api";
 
 
@@ -8,6 +8,7 @@ const RegisterRoutes = (app) => {
     app.get('/get_countries', CountryList);
     app.get('/get_countries/worldwide', WorldWideData);
     app.get('/get_countries/:country', CountryData);
+    app.get('/historic/:country', HistoricData);
 };
 
 export default RegisterRoutes;
