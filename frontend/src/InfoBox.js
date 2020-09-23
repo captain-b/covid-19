0f07fe6d1f20514525b8e8a47cb22c66f326f820
Captain-b <babakrezai@hotmail.com>
2020-09-23 01:47:50 +0100

    Change background color when a cases is selected

--- frontend/src/InfoBox.js
+++ frontend/src/InfoBox.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Card, CardContent, Typography} from '@material-ui/core';
 import './InfoBox.css'
 
-function InfoBox({title, cases, totalCases, isPurple, isRed, isGreen, active, ...props}) {
+function InfoBox({backgroundColor, title, cases, totalCases, isPurple, isRed, isGreen, active, ...props}) {
 	return (
-		<Card onClick={props.onClick} className={`infoBox ${active && 'infoBox--selected'} ${isRed && 'infoBox--red'} ${isPurple && 'infoBox--purple'}`}>
+		<Card style={{backgroundColor: backgroundColor}} onClick={props.onClick} className={`infoBox ${active && 'infoBox--selected'} ${isRed && 'infoBox--red'} ${isPurple && 'infoBox--purple'}`}>
 			<CardContent>
 				<Typography className="infoBox__title" color="textSecondary">
 					{title}
